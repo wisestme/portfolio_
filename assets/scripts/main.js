3f8a408d65697359bb1bb281713d4683cb60f8a9
wisestme <wyhzest@gmail.com>
2022-01-19 17:04:44 +0100

    add navigation modal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,13 @@
+// Show Hamburger Menu
+let hamburgerMenu = document.querySelector('.hamburger_menu');
+let navigationModal = document.querySelector('.navigation_modal');
+if(hamburgerMenu) {
+    hamburgerMenu.addEventListener('click', () => {
+        navigationModal.style.top = 0;
+    })
+}
+
+// show soon modal
 let allSoon = document.querySelectorAll('.soon');
 let soonModal = document.querySelector('#soon_modal');
 console.log(allSoon)
@@ -9,3 +19,4 @@ if(allSoon) {
         })
     });
 }
+
