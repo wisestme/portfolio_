fda2d4054480a5103ecb4087574fb4d6cbed2a80
wisestme <wyhzest@gmail.com>
2022-01-24 13:05:58 +0100

    control modals

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,13 @@
+// MODAL
+let modal = document.querySelectorAll('.modal');
+
 // Show Hamburger Menu
 let hamburgerMenu = document.querySelector('.hamburger_menu');
 let navigationModal = document.querySelector('.navigation_modal');
 if(hamburgerMenu) {
     hamburgerMenu.addEventListener('click', () => {
         navigationModal.style.top = 0;
+        showCloseIcon();
     })
 }
 
@@ -16,17 +20,37 @@ if(allSoon) {
     allSoon.forEach(soon => {
         soon.addEventListener('click', () => {
             soonModal.style.top = 0;
-            modalCloseIcon.style.display = 'block';
+            showCloseIcon();
         })
     });
 }
 
+// Hide modal
+function hideModal() {
+    modal.forEach(element => {
+        element.style.top = '-100vh'
+    });
+}
+
+// Close icon
+
 // Close modal
 let modalCloseIcon = document.querySelector('.fa-times');
 
+// Hide close icon
+function hideCloseIcon() {
+    modalCloseIcon.style.display = 'none';
+}
+
+// Show close icon
+function showCloseIcon() {
+    modalCloseIcon.style.display = 'block';
+}
+
 if(modalCloseIcon) {
     modalCloseIcon.addEventListener('click', () => {
-        alert('Close modal')
+        hideModal();
+        hideCloseIcon();
     })
 }
 
